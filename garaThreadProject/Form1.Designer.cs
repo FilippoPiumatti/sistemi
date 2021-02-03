ed90a98f87e9d3fd1cf7031d89688165e054b352
filippo piumatti <f.piumatti.1365@vallauri.edu>
2021-02-03 21:54:10 +0100

    macchine con thread

---
+++ garaThreadProject/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace _02_Automobiline_Thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(133, 329);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 57);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 381);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(859, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(133, 392);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(75, 57);
+            this.btnAbort.TabIndex = 8;
+            this.btnAbort.Text = "ABORT";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::_02_Automobiline_Thread.Properties.Resources.seletti_lampada_led_linea_rossa_myho_3_;
+            this.pictureBox6.InitialImage = global::_02_Automobiline_Thread.Properties.Resources.seletti_lampada_led_linea_rossa_myho_3_;
+            this.pictureBox6.Location = new System.Drawing.Point(978, 39);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(12, 335);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::_02_Automobiline_Thread.Properties.Resources._000_1191186_GOLF_MINI_AUTO_ELETTRICA_PER_BAMBINI;
+            this.pictureBox5.InitialImage = global::_02_Automobiline_Thread.Properties.Resources._000_1191186_GOLF_MINI_AUTO_ELETTRICA_PER_BAMBINI;
+            this.pictureBox5.Location = new System.Drawing.Point(28, 263);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::_02_Automobiline_Thread.Properties.Resources.th__7_1;
+            this.pictureBox4.InitialImage = global::_02_Automobiline_Thread.Properties.Resources.th__7_1;
+            this.pictureBox4.Location = new System.Drawing.Point(28, 207);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::_02_Automobiline_Thread.Properties.Resources.th__6_;
+            this.pictureBox3.InitialImage = global::_02_Automobiline_Thread.Properties.Resources.th__6_;
+            this.pictureBox3.Location = new System.Drawing.Point(28, 151);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_02_Automobiline_Thread.Properties.Resources.macchine_usate;
+            this.pictureBox2.InitialImage = global::_02_Automobiline_Thread.Properties.Resources.macchine_usate;
+            this.pictureBox2.Location = new System.Drawing.Point(28, 95);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_02_Automobiline_Thread.Properties.Resources.th__5_;
+            this.pictureBox1.InitialImage = global::_02_Automobiline_Thread.Properties.Resources.th__5_;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 450);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAbort;
+    }
+}
+
---
+++ garaThreadProject/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _02_Automobiline_Thread
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region consegna
+        /// 5 picture box, ognuna è una macchinina,
+        /// a ogni macchinina associo un thread
+        /// 1 unico metodo per automobile 
+        /// OBBIETTIVO: Arrivare alla riga rossa posta a 900px orizzontali
+        /// ogni machinina a una velocità diversa in px/sec generato casualmente tra 5px/sec e 30px/sec
+        /// stampare in una textbox l'ordine di arrivo
+#endregion
+
+        private struct str
+        {
+            public int velocita;
+            public Thread auto;
+            public bool ar;
+            public PictureBox pb;
+            public int pos;
+        }
+        List<str> lst = new List<str>();
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            for (int i = 0; i < 5; i++)
+            {
+                str st = new str();
+                st.auto = new Thread(threadAuto);
+                st.velocita = rnd.Next(5, 30);
+                st.ar = false;
+                st.pos = i + 1;
+                switch (i)
+                {
+                    case 0:
+                        st.pb = pictureBox1;
+                        break;
+                    case 1:
+                        st.pb = pictureBox2;
+                        break;
+                    case 2:
+                        st.pb = pictureBox3;
+                        break;
+                    case 3:
+                        st.pb = pictureBox4;
+                        break;
+                    case 4:
+                        st.pb = pictureBox5;
+                        break;
+                }
+                lst.Add(st);
+            }
+        }
+
+        private void threadAuto(object obj)
+        {
+            str s = (str)obj;
+            while (!s.ar)
+            {
+                if (s.pb.Location.X > 900)
+                {
+                    BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        s.ar = true;
+                        textBox1.Text += "macchina " + s.pos;
+                    });
+                }
+                else {
+                    BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        s.pb.Left += s.velocita;
+                    });
+                    Thread.Sleep(100);
+                }
+                
+            }
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                lst[i].auto.Start(lst[i]);
+            }
+        }
+
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                lst[i].auto.Abort();
+            }
+        }
+    }
+}
